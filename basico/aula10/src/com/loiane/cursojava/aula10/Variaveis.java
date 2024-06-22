c1d27f09cad3c19f03a51ae704e865704c6bd741
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-06-22 17:38:22 -0300

    aula10 criada

---
+++ basico/aula10/src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 18;
+		String nome = "Pablo";
+		
+		idade = 19;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+	}
+
+}
