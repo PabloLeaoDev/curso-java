1730995af4ce3c30c797c8d0894b3be19a85b864
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-07-17 23:07:45 -0300

    OperadoresAritmeticos.java criado

---
+++ basico/src/com/loiane/cursojava/aula13/OperadoresAritmeticos.java
@@ -0,0 +1,27 @@
+package com.loiane.cursojava.aula13;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado--;
+		System.out.println(resultado);
+		
+		resultado *= 2;
+		System.out.println(resultado);
+		
+		resultado /= 2;
+		System.out.println(resultado);
+		
+		resultado += 8;
+		System.out.println(resultado);
+		
+		resultado %= 7;
+		System.out.println(resultado);
+		
+		System.out.println(++resultado);
+	}
+
+}
